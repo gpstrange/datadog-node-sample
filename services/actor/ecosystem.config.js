8882d1ab06e03be01a46d8a0412e108463ea5ad3
gpstrange <prasadguru141@gmail.com>
2025-04-08 16:49:52 +0530

    Add ecosystem configuration files for actor and movie services

---
+++ services/actor/ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    apps: [
+      {
+        name: "actor",
+        exec_mode: "cluster",
+        instances: "1",
+        script: "./dist/main.js", // your script
+        args: "start",
+        env: {
+            OTEL_SERVICE_NAME: "actor",
+            OTEL_RESOURCE_ATTRIBUTES: "kubesense.env_type=legacy,kubesense.cluster=pi42",
+            OTEL_EXPORTER_OTLP_TRACES_ENDPOINT: "http://dev.kubesense.ai:33443/v1/traces",
+            OTEL_NODE_RESOURCE_DETECTORS: "env,host,os,process",
+            NODE_OPTIONS: "--require @opentelemetry/auto-instrumentations-node/register"
+        },
+      },
+    ],
+  };
